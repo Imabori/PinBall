8a22499f11022638457b943815ad9e1d47cee0c5
今堀耕一 <mokka_dogonsui@yahoo.co.jp>
2017-04-21 18:23:04 +0900

    プロジェクトPinBall　課題作成
    Lesson5の課題：得点の表示　
    星、雲と衝突した際の得点取得、画面右上に表示

---
+++ Assets/GemePoint.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemePoint : MonoBehaviour {
+
+    //ポイント表示するテキスト
+    private GameObject GamePointText;
+
+    //ポイントの変数宣言
+    int point = 0;
+    
+    // Use this for initialization
+    void Start () {
+
+        //シーン中のGamePointTextオブジェクトを取得
+        this.GamePointText = GameObject.Find("GamePointText");
+
+        //初期ポイントを表示
+        this.GamePointText.GetComponent<Text>().text = "Point : " + point;
+        }
+
+    // Update is called once per frame
+    void Update() {
+    
+    }
+
+    //星、雲に当たった時にポイントを加算する関数
+    void OnCollisionEnter(Collision other) { 
+            //小さい星と衝突した？
+            if (other.gameObject.tag == "SmallStarTag"){
+            point += 10;
+    
+            //大きい星と衝突した？
+            }else if (other.gameObject.tag == "LargeStarTag"){
+            point += 20;
+
+            //小さい雲と衝突した？
+            }else if (other.gameObject.tag == "SmallCloudTag"){
+            point += 30;
+            
+            //大きい雲と衝突した？
+            }else if (other.gameObject.tag == "LargeCloudTag"){
+            point += 50;
+            }
+
+
+
+        //初期ポイントを表示
+        this.GamePointText.GetComponent<Text>().text = "Point : " + point;
+    }
+}
